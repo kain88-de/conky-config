45ba4b402bb3336d2190e0f1f395d9147c9d5997
Max Linke <max_linke@gmx.de>
2015-01-18 23:20:24 +0100

    Initial commit

---
+++ emacs.el
@@ -0,0 +1,5 @@
+;; minimal emacs config to write the agenda to stdout
+(custom-set-variables
+ '(org-agenda-files (quote ("~/org/organizer.org")))
+ '(org-agenda-start-on-weekday nil)
+ '(org-agenda-ndays 7))
---
+++ start_conky.sh
@@ -0,0 +1,3 @@
+conky -dc .conky/conkyrc &
+conky -dc .conky/conkyrc_org &
+conky -dc .conky/conkyrc_shortcuts &
