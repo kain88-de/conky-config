7c8d07ff4412eaa496391afb32c7566a5522cf70
Max Linke <max_linke@gmx.de>
2015-01-19 10:55:02 +0100

    Add conky desktop file to use in .config/autostart
    Gnome 3.12 does not provide a UI anymore to start arbitrary shell
    commands anymore. The best way to achieve this now is to use desktop
    files.

--- start_conky.sh
+++ start_conky.sh
@@ -1,3 +1,5 @@
-conky -dc .conky/conkyrc &
-conky -dc .conky/conkyrc_org &
-conky -dc .conky/conkyrc_shortcuts &
+#!/bin/bash
+
+conky -dc $HOME/.conky/conkyrc &
+conky -dc $HOME/.conky/conkyrc_org &
+conky -dc $HOME/.conky/conkyrc_shortcuts &
