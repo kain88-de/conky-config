06f083dc6221b243fd2b6f269ce751b7d45aec66
Max Linke <max_linke@gmx.de>
2015-01-26 21:28:27 +0100

    update org-todo list conky

--- emacs.el
+++ emacs.el
@@ -2,4 +2,6 @@
 (custom-set-variables
  '(org-agenda-files (quote ("~/org/organizer.org")))
  '(org-agenda-start-on-weekday nil)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-scheduled-if-done t)
  '(org-agenda-ndays 7))
