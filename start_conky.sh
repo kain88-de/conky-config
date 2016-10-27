23fdfe477f3474f8a5d831dffaf22099f3fe3ae9
Max Linke <max_linke@gmx.de>
2016-10-27 11:20:52 +0200

    update to new harddrive and wallpaper

--- start_conky.sh
+++ start_conky.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 conky -dc $HOME/.conky/conkyrc &
-conky -dc $HOME/.conky/conkyrc_org &
-conky -dc $HOME/.conky/conkyrc_shortcuts &
+# conky -dc $HOME/.conky/conkyrc_org &
+# conky -dc $HOME/.conky/conkyrc_shortcuts &
